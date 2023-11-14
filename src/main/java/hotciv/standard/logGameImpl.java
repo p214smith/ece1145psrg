19e356ed6fb20fc15c653b5155aba43db1e7c0af
p214smith <77897203+p214smith@users.noreply.github.com>
2023-11-13 19:24:39 -0500

    Implemented logging functionality

---
+++ src/main/java/hotciv/standard/logGameImpl.java
@@ -0,0 +1,86 @@
+package hotciv.standard;
+import hotciv.framework.*;
+
+
+import java.util.*;
+public class logGameImpl implements Game {
+    public logGameImpl(Factory factory){
+        this.game = new GameImpl(factory);
+        this.logging = true;
+    }
+    protected boolean logging;
+    protected Game game;
+
+    public Tile getTileAt(Position p) {
+        return this.game.getTileAt(p);
+    }
+    public Unit getUnitAt(Position p) {
+        return this.game.getUnitAt(p);
+    }
+    public City getCityAt(Position p) {
+        return this.game.getCityAt(p);
+    }
+    public boolean moveUnit(Position from, Position to) {
+        if (game.moveUnit(from,to)){
+            if(this.logging){
+                Player play = game.getPlayerInTurn();
+                Unit unit = game.getUnitAt(to);
+                String s = play.toString() + " " + unit.getTypeString() + " from " + from.toString() + " to " + to.toString() + ".";
+                System.out.println(s);}
+            return true;}
+        else
+            return false;
+    }
+    public Player getPlayerInTurn() {
+        return game.getPlayerInTurn();
+    }
+    public Player getWinner() {
+        return game.getWinner();
+    }
+    public void changeProductionInCityAt(Position p, String unitType) {
+        City unit = game.getCityAt(p);
+        if(Objects.nonNull(unit)) {
+            if (this.logging) {
+                Player play = this.game.getPlayerInTurn();
+                String s = play.toString() + " changes production in city at " + p.toString() + " to " + unitType + ".";
+                System.out.println(s);
+            }
+        }
+        game.changeProductionInCityAt(p,unitType);
+    }
+    public void changeWorkForceFocusInCityAt(Position p, String balance) {
+        City unit = game.getCityAt(p);
+        if(Objects.nonNull(unit)) {
+            if (this.logging) {
+                Player play = this.game.getPlayerInTurn();
+                String s = play.toString() + " changes workforce focus in city at " + p.toString() + " to " + balance + ".";
+                System.out.println(s);
+            }
+        }
+        game.changeWorkForceFocusInCityAt(p,balance);
+    }
+    public int getAge() {
+        return game.getAge();
+    }
+    public void performUnitActionAt(Position p) {
+        Unit unit = game.getUnitAt(p);
+        if(Objects.nonNull(unit)){
+            if (this.logging){
+                Player play = this.game.getPlayerInTurn();
+                String s = play.toString() + " performed unit action on " + unit.getTypeString() + " at position " + p.toString() + ".";
+                System.out.println(s);
+            }
+        }
+        game.performUnitActionAt(p);
+    }
+    public void endOfTurn() {
+        Player play =game.getPlayerInTurn();
+        if (this.logging){
+            String s = play.toString() + " ends turn.";
+            System.out.println(s);}
+        game.endOfTurn();
+    }
+    public void toggleLogging(){
+        this.logging = !this.logging;
+    }
+}
---
+++ src/test/java/hotciv/standard/testLoggingGameImpl.java
@@ -0,0 +1,185 @@
+package hotciv.standard;
+import hotciv.framework.*;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+public class testLoggingGameImpl {
+    private Game game;
+    @Before
+    public void setUp() {
+        game = new logGameImpl(new testFactory());
+    }
+
+    @Test
+    public void shouldBeRedAsStartingPlayer() {
+        assertThat(game, is(notNullValue()));
+        assertThat(game.getPlayerInTurn(), is(Player.RED));
+    }
+    @Test
+    public void CitiesAtRightPosition() {
+        City test_City = game.getCityAt(new Position(8,12));
+        assertThat(test_City.getOwner(), is(Player.RED));
+        test_City = game.getCityAt(new Position(4,5));
+        assertThat(test_City.getOwner(), is(Player.BLUE));
+    }
+    @Test
+    public void checkTerrainTileLocations() {
+        assertThat (game, is(notNullValue()));
+        assertThat (game.getTileAt(new Position(7,10)).getTypeString(), is(GameConstants.HILLS));
+        assertThat (game.getTileAt(new Position(1,11)).getTypeString(), is(GameConstants.FOREST));
+        assertThat (game.getTileAt(new Position(7,13)).getTypeString(), is(GameConstants.MOUNTAINS));
+        assertThat(game.getTileAt(new Position(9,8)).getTypeString(), is(GameConstants.OCEANS));
+        assertThat(game.getTileAt(new Position(13,7)).getTypeString(), is(GameConstants.PLAINS));
+    }
+    @Test
+    public void checkUnitLocations() {
+        assertThat (game, is(notNullValue()));
+        Unit tester = game.getUnitAt(new Position(3,8));
+        assertThat (tester.getTypeString(), is(GameConstants.ARCHER));
+        tester = game.getUnitAt(new Position(4,4));
+        assertThat (tester.getTypeString(), is(GameConstants.LEGION));
+        tester = game.getUnitAt(new Position(5,5));
+        assertThat (tester.getTypeString(), is(GameConstants.SETTLER));
+    }
+    @Test
+    public void checkUnitOwner() {
+        assertThat (game, is(notNullValue()));
+        Unit tester = game.getUnitAt(new Position(3,8));
+        assertThat (tester.getOwner(), is(Player.RED));
+        tester = game.getUnitAt(new Position(4,4));
+        assertThat (tester.getOwner(), is(Player.BLUE));
+        tester = game.getUnitAt(new Position(5,5));
+        assertThat (tester.getOwner(), is(Player.RED));
+    }
+    @Test
+    public void gameAgesProperly(){
+        assertThat (game, is(notNullValue()));
+        int age = -4000;
+        for(int i = 0; i < (4000-100)/100;i++){
+            assertThat(game.getAge(),is(age));
+            game.endOfTurn();
+            game.endOfTurn();
+            age +=100;
+        }
+        assertThat(game.getAge(),is(age));
+        game.endOfTurn();
+        game.endOfTurn();
+        age +=99;
+        assertThat(game.getAge(),is(age));
+        game.endOfTurn();
+        game.endOfTurn();
+        age +=1;
+        assertThat(game.getAge(),is(age));
+        game.endOfTurn();
+        game.endOfTurn();
+        age +=1;
+        assertThat(game.getAge(),is(age));
+        game.endOfTurn();
+        game.endOfTurn();
+        age +=49;
+        for(int i = 0; i < (1750 - 50)/50;i++){
+            assertThat(game.getAge(),is(age));
+            game.endOfTurn();
+            game.endOfTurn();
+            age +=50;
+        }
+        for(int i = 0; i < (1900 - 1750)/25;i++){
+            assertThat(game.getAge(),is(age));
+            game.endOfTurn();
+            game.endOfTurn();
+            age +=25;
+        }
+        for(int i = 0; i < (1970 - 1900)/5;i++){
+            assertThat(game.getAge(),is(age));
+            game.endOfTurn();
+            game.endOfTurn();
+            age +=5;
+        }
+        for(int i = 0; i < 30;i++){
+            assertThat(game.getAge(),is(age));
+            game.endOfTurn();
+            game.endOfTurn();
+            age +=1;
+        }
+    }
+    @Test
+    public void test_unit_movement(){
+        assertThat (game, is(notNullValue()));
+        assertTrue(game.moveUnit(new Position(3,8),new Position(3,9)));
+        assertThat(game.getUnitAt(new Position(3,9)).getMoveCount(),is(0));
+        assertFalse(game.moveUnit(new Position(3,9),new Position(3,8)));
+        assertTrue(game.moveUnit(new Position(5,5),new Position(5,4)));
+        assertThat(game.getUnitAt(new Position(5,4)).getMoveCount(),is(0));
+        game.endOfTurn();
+        assertFalse(game.moveUnit(new Position(4,4),new Position(3,4)));
+        assertTrue(game.moveUnit(new Position(4,4),new Position(4,5)));
+
+        assertThat(game.getUnitAt(new Position(4,5)).getMoveCount(),is(0));
+        game.endOfTurn();
+        assertFalse(game.moveUnit(new Position(4,5),new Position(4,4)));
+        assertFalse(game.moveUnit(new Position(3,9),new Position(3,10)));
+        assertFalse(game.moveUnit(new Position(3,9),new Position(1,9)));
+        assertTrue(game.moveUnit(new Position(3,9),new Position(3,8)));
+        assertThat(game.getUnitAt(new Position(3,8)).getMoveCount(),is(0));
+        assertTrue(game.moveUnit(new Position(5,4),new Position(4,3)));
+        assertThat(game.getUnitAt(new Position(4,3)).getMoveCount(),is(0));
+        assertThat(game.getUnitAt(new Position(4,3)).getTypeString(),is(GameConstants.SETTLER));
+        game.endOfTurn();
+        assertTrue(game.moveUnit(new Position(4,5),new Position(4,4)));
+    }
+    @Test
+    public void test_winning_condition(){
+        assertThat (game, is(notNullValue()));
+        assertThat(game.getWinner(),is(nullValue()));
+        assertTrue(game.moveUnit(new Position(5,5),new Position(4,5)));
+        assertThat(game.getUnitAt(new Position(4,5)).getMoveCount(),is(0));
+        assertThat(game.getCityAt(new Position(4,5)).getOwner(),is(Player.RED));
+        game.endOfTurn();
+        assertThat(game.getWinner(),is(Player.RED));
+
+    }
+    @Test
+    public void test_unit_actions(){
+        assertThat (game, is(notNullValue()));
+        game.performUnitActionAt(new Position(3,8));
+        assertThat(game.getUnitAt(new Position(3,8)).getDefensiveStrength(),is(6));
+        assertThat(game.getUnitAt(new Position(3,8)).getMoveCount(), is(0));
+        game.endOfTurn();
+        game.endOfTurn();
+        assertThat(game.getUnitAt(new Position(3,8)).getDefensiveStrength(),is(6));
+        assertThat(game.getUnitAt(new Position(3,8)).getMoveCount(), is(0));
+        game.performUnitActionAt(new Position(3,8));
+        assertThat(game.getUnitAt(new Position(3,8)).getDefensiveStrength(),is(3));
+        game.performUnitActionAt(new Position(5,5));
+        assertThat(game.getCityAt(new Position(5,5)).getOwner(),is(Player.RED));
+    }
+    @Test
+    public void test_unit_creation(){
+        assertThat (game, is(notNullValue()));
+        for(int i = 0;i < 40;i++)
+            game.endOfTurn();
+        assertThat(game.getUnitAt(new Position(3,6)).getOwner(),is(Player.BLUE));
+        assertThat(game.getUnitAt(new Position(4,6)).getOwner(),is(Player.BLUE));
+        assertThat(game.getUnitAt(new Position(5,6)).getOwner(),is(Player.BLUE));
+        assertThat(game.getUnitAt(new Position(5,5)).getOwner(),is(Player.RED));
+        assertThat(game.getUnitAt(new Position(5,4)).getOwner(),is(Player.BLUE));
+        assertThat(game.getUnitAt(new Position(4,4)).getOwner(),is(Player.BLUE));
+        assertThat(game.getCityAt(new Position(4,5)).getTreasury(),is((20*6)-(4*15)));
+        assertThat(game.getUnitAt(new Position(7,12)).getOwner(),is(Player.RED));
+        assertThat(game.getUnitAt(new Position(8,13)).getOwner(),is(Player.RED));
+        assertThat(game.getUnitAt(new Position(9,13)).getOwner(),is(Player.RED));
+        assertThat(game.getUnitAt(new Position(9,12)).getOwner(),is(Player.RED));
+        assertThat(game.getUnitAt(new Position(9,11)).getOwner(),is(Player.RED));
+        assertThat(game.getUnitAt(new Position(8,11)).getOwner(),is(Player.RED));
+        assertThat(game.getUnitAt(new Position(7,11)).getOwner(),is(Player.RED));
+        assertThat(game.getCityAt(new Position(8,12)).getTreasury(),is((20*6)-(7*15)));
+        game.endOfTurn();
+        game.changeProductionInCityAt(new Position(4,5),GameConstants.SETTLER);
+        game.moveUnit(new Position(5,6),new Position(5,5));
+        ((logGameImpl)game).toggleLogging();
+        game.endOfTurn();
+        assertThat(game.getUnitAt(new Position(5,6)).getTypeString(),is(GameConstants.SETTLER));
+
+    }
+}
